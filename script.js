5d0b9c93a8ccdb15a4a66a5ec32272f6f3d7689b
Flavio <flavioescalegari@gmail.com>
2024-04-03 14:55:42 -0600

    adding bg img

--- script.js
+++ script.js
@@ -5,73 +5,57 @@ const confirmName = document.getElementById('confirmName');
 const confirmAddress = document.getElementById('confirmAddress');
 const confirmStatus = document.getElementById('confirmStatus');
 const confirmFee = document.getElementById('confirmFee');
-const buttonBack = document.getElementById('buttonBack');
+const backButton = document.getElementById('backButton');
 
 registrationForm.addEventListener('submit', (event) => {
-    event.preventDefault(); 
+  event.preventDefault();
 
-    const formData = new FormData(registrationForm);
-    const userID = formData.get('userID');
-    const fullName = formData.get('fullName');
-    const address = formData.get('address');
-    const status = formData.get('status');
-    
-  
-    const apiEndpoint = 'https://web-bvc-sport-club-0a7m.onrender.com/api/register'; 
-    //const apiEndpoint = 'http://localhost:3000/api/register'; 
+  const formData = new FormData(registrationForm);
+  const memberID = formData.get('memberID');
+  const memberFullName = formData.get('memberFullName');
+  const memberAddress = formData.get('memberAddress');
+  const memberStatus = formData.get('memberStatus');
 
-    fetch(apiEndpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json' 
-      },
-        body: JSON.stringify({
-          userID, fullName, address, status
-        }) 
+  const apiEndpoint = 'https://web-bvc-sport-club-0a7m.onrender.com/api/register'; 
+
+  fetch(apiEndpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      memberID, memberFullName, memberAddress, memberStatus 
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-        
-    })
-    .then(data => { 
-      const formData = new FormData(registrationForm);
-      
-      const registrationData = {
-        id: formData.get('userID'),
-        fullName: formData.get('fullName'),
-        address: formData.get('address'),
-        status: formData.get('status'),
-      };
-    
-      confirmID.textContent = "ID: " + registrationData.id;
-      confirmName.textContent = "NAME: " + registrationData.fullName;
-      confirmAddress.textContent = "ADDRESS: " + registrationData.address;
-      confirmStatus.textContent = "STATUS: " + registrationData.status;
-      confirmFee.textContent = "FEE: " + data.fee;
-    
-      confirmationDiv.style.display = 'block'; 
-      registrationForm.style.display = 'none';
-      buttonBack.style.display = 'block'; 
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); 
+  })
+  .then(data => { 
+    confirmID.textContent = "ID: " + memberID;
+    confirmName.textContent = "NAME: " + memberFullName;
+    confirmAddress.textContent = "ADDRESS: " + memberAddress;
+    confirmStatus.textContent = "STATUS: " + memberStatus;
+    confirmFee.textContent = "FEE: " + data.fee;
 
-      buttonBack.addEventListener("click", backButton);
-      function backButton() {
-        confirmationDiv.style.display = 'none'; 
-        registrationForm.style.display = 'block';
-        buttonBack.style.display = 'none'; 
-        clearForm();
-      }
-
-      function clearForm() {
-        document.getElementById("registrationForm").reset();
-      }
-      
-      
-    })
-    .catch(error => {
-        console.error('Error submitting form:', error);
-    });
-    
+    confirmationDiv.style.display = 'block'; 
+    registrationForm.style.display = 'none';
+    backButton.style.display = 'block'; 
+  })
+  .catch(error => {
+    console.error('Error submitting form:', error);
+  });
 });
+
+backButton.addEventListener("click", () => { 
+  confirmationDiv.style.display = 'none'; 
+  registrationForm.style.display = 'block';
+  backButton.style.display = 'none'; 
+  clearForm();
+});
+
+function clearForm() {
+  document.getElementById("registrationForm").reset();
+}
